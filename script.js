079b9632f44064d1c80927e20d23728fcdca3095
Your Name <emil.dob.098@gmail.com>
2023-04-20 01:34:47 +0200

    ee

--- script.js
+++ script.js
@@ -115,8 +115,8 @@ class Calculator {
   }
   showVersion() {
     this.displaySmall.innerHTML = 'Version';
-    this.displayBig.innerHTML = '1.7.1';
-    console.log('v1.7.1');
+    this.displayBig.innerHTML = '1.7.2';
+    console.log('v1.7.2');
   }
   backspaceLong() {
     this.displayBig.innerHTML = 0;
