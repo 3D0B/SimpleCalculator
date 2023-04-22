736e2baba43d02224d980a7143063a25ba7e35a5
Your Name <emil.dob.098@gmail.com>
2023-04-22 09:22:42 +0200

    ed

--- script.js
+++ script.js
@@ -115,8 +115,8 @@ class Calculator {
   }
   showVersion() {
     this.displaySmall.innerHTML = 'Version';
-    this.displayBig.innerHTML = '1.8.3';
-    console.log('v1.8.3');
+    this.displayBig.innerHTML = '1.8.4';
+    console.log('v1.8.4');
   }
   backspaceLong() {
     this.displayBig.innerHTML = 0;
