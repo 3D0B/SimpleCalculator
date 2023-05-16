1a7323dea03d3f4db2fc9b60b1d120ff38eb496a
Your Name <emil.dob.098@gmail.com>
2023-05-17 00:32:46 +0200

    d

--- sw.js
+++ sw.js
@@ -1,42 +1,21 @@
-
-var staticCacheName = 'static-cache-v1';
-
-const filesToCache = [
+const cacheName = 'my-app-cache-v1';
+const staticAssets = [
     "./index.html",
     "./script.js",
-    "./style.css"
+    "./style.css",
+    // Dodaj tutaj wszystkie statyczne zasoby Twojej aplikacji, które mają być przechowywane w pamięci podręcznej
 ];
 
-self.addEventListener('install', function (event) {
+self.addEventListener('install', (event) => {
     event.waitUntil(
-        caches.open(staticCacheName).then(function (cache) {
-            return cache.addAll(filesToCache);
-        })
+        caches.open(cacheName)
+            .then((cache) => cache.addAll(staticAssets))
     );
 });
 
-self.addEventListener('fetch', function (event) {
+self.addEventListener('fetch', (event) => {
     event.respondWith(
-        caches.match(event.request).then(function (response) {
-            return response || fetch(event.request);
-        })
+        caches.match(event.request)
+            .then((response) => response || fetch(event.request))
     );
 });
-self.addEventListener("activate", event => {
-    console.log("Service worker activated");
-});
-
-self.addEventListener('activate', function (event) {
-    event.waitUntil(
-        caches.keys().then(function (cacheNames) {
-            return Promise.all(
-                cacheNames.filter(function (cacheName) {
-                    return cacheName.startsWith('static-cache-') && cacheName !== staticCacheName;
-                }).map(function (cacheName) {
-                    return caches.delete(cacheName);
-                })
-            );
-        })
-    );
-});
-
