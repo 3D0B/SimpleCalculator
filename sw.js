5968a54a0bdfc4bcb9fc17fb4552662c7e6660af
Your Name <emil.dob.098@gmail.com>
2023-05-17 10:45:54 +0200

    e

--- sw.js
+++ sw.js
@@ -1,21 +1,82 @@
-const cacheName = 'my-app-cache-v1';
-const staticAssets = [
-    "./index.html",
-    "./script.js",
-    "./style.css",
-    // Dodaj tutaj wszystkie statyczne zasoby Twojej aplikacji, które mają być przechowywane w pamięci podręcznej
-];
+
+
+const addResourcesToCache = async (resources) => {
+    const cache = await caches.open('v1');
+    await cache.addAll(resources);
+};
+
+const putInCache = async (request, response) => {
+    const cache = await caches.open('v1');
+    await cache.put(request, response);
+};
+
+const cacheFirst = async ({ request, preloadResponsePromise, fallbackUrl }) => {
+    // First try to get the resource from the cache
+    const responseFromCache = await caches.match(request);
+    if (responseFromCache) {
+        return responseFromCache;
+    }
+
+    // Next try to use the preloaded response, if it's there
+    const preloadResponse = await preloadResponsePromise;
+    if (preloadResponse) {
+        console.info('using preload response', preloadResponse);
+        putInCache(request, preloadResponse.clone());
+        return preloadResponse;
+    }
+
+    // Next try to get the resource from the network
+    try {
+        const responseFromNetwork = await fetch(request);
+        // response may be used only once
+        // we need to save clone to put one copy in cache
+        // and serve second one
+        putInCache(request, responseFromNetwork.clone());
+        return responseFromNetwork;
+    } catch (error) {
+        const fallbackResponse = await caches.match(fallbackUrl);
+        if (fallbackResponse) {
+            return fallbackResponse;
+        }
+        // when even the fallback response is not available,
+        // there is nothing we can do, but we must always
+        // return a Response object
+        return new Response('Network error happened', {
+            status: 408,
+            headers: { 'Content-Type': 'text/plain' },
+        });
+    }
+};
+
+const enableNavigationPreload = async () => {
+    if (self.registration.navigationPreload) {
+        // Enable navigation preloads!
+        await self.registration.navigationPreload.enable();
+    }
+};
+
+self.addEventListener('activate', (event) => {
+    event.waitUntil(enableNavigationPreload());
+});
 
 self.addEventListener('install', (event) => {
     event.waitUntil(
-        caches.open(cacheName)
-            .then((cache) => cache.addAll(staticAssets))
+        addResourcesToCache([
+            '/',
+            '/index.html',
+            "./index.html",
+            "./script.js",
+            "./style.css",
+        ])
     );
 });
 
 self.addEventListener('fetch', (event) => {
     event.respondWith(
-        caches.match(event.request)
-            .then((response) => response || fetch(event.request))
+        cacheFirst({
+            request: event.request,
+            preloadResponsePromise: event.preloadResponse,
+            
+        })
     );
 });
