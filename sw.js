9089be8c57309dca32b769ac032f05595bdac153
Your Name <emil.dob.098@gmail.com>
2023-05-18 00:37:25 +0200

    dd

--- sw.js
+++ sw.js
@@ -1,10 +1,10 @@
 const CACHE_NAME = 'my-app-cache-v1';
 const urlsToCache = [
     '/',
-    './index.html',
-    './style.css',
-    './script.js',
-    './manifest.json'
+    '/SimpleCalculator/index.html',
+    '/SimpleCalculator/style.css',
+    '/SimpleCalculator/script.js',
+    '/SimpleCalculator/manifest.json'
 ];
 
 self.addEventListener('install', (event) => {
