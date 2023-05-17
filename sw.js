46dc12c4f915f79bf9816ee7717190b1760f8bcc
Your Name <emil.dob.098@gmail.com>
2023-05-17 22:57:55 +0200

    kkkk

--- sw.js
+++ sw.js
@@ -1,30 +1,35 @@
 const CACHE_NAME = 'my-app-cache-v1';
 const urlsToCache = [
-  './',
-  './index.html',
-  './style.css',
-  './script.js',
-  './manifest.json',
-  './Icons/Mobile/2.0/192.png',
-  './Icons/Mobile/2.0/512.png',
-  './Icons/desktop/2.0/192.png',
-  './Icons/desktop/2.0/512.png'
+    './',
+    './index.html',
+    './style.css',
+    './script.js',
+    './manifest.json',
+    './Icons/Mobile/2.0/192.png',
+    './Icons/Mobile/2.0/512.png',
+    './Icons/desktop/2.0/192.png',
+    './Icons/desktop/2.0/512.png'
 ];
 
 self.addEventListener('install', (event) => {
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then((cache) => {
-        return cache.addAll(urlsToCache);
-      })
-  );
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then((cache) => {
+                return cache.addAll(urlsToCache);
+            })
+    );
 });
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+        self.skipWaiting()
+    );
+})
 
 self.addEventListener('fetch', (event) => {
-  event.respondWith(
-    caches.match(event.request)
-      .then((response) => {
-        return response || fetch(event.request);
-      })
-  );
+    event.respondWith(
+        caches.match(event.request)
+            .then((response) => {
+                return response || fetch(event.request);
+            })
+    );
 });
